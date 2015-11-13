1a47611c4d51ddea7f3b7221713f9066cd134a7f
Erik Sõlg <eriks1995@gmail.com>
2015-11-13 23:48:48 +0200

    Kodutöö

---
+++ src/harjutused/Harjutus.java
@@ -0,0 +1,14 @@
+package harjutused;
+
+
+public abstract class Harjutus {
+
+    public String harjutuseKirjeldus;
+    public int harjutuseJark;
+    public boolean kasViimane;
+
+    public void kaivitaUlesanne() {
+
+    }
+
+}
---
+++ src/main/MainPanel.java
@@ -0,0 +1,47 @@
+//package main;
+//import javax.swing.*;
+//import java.awt.*;
+//import java.awt.event.*;
+//import java.awt.font.*;
+//
+//public class MainPanel extends JPanel {
+//
+//    public MainPanel() {
+//
+//    }
+//
+//    public void toExercises() {
+//        repaint();
+//    }
+//
+//    public void paintComponent(Graphics g) {
+//        setBackground(new Color(0.8F, 0.8F, 1.0F));
+//        super.paintComponent(g);
+//        g.setColor(new Color(200,0, 0));
+//        g.setFont(new Font("Serif", Font.BOLD, 42));
+//        g.drawString("Matemaatika õpituba", 390, 100);
+//    }
+//
+//    public class MainListener extends MouseAdapter {
+//
+//        public void mousePressed(MouseEvent evt) {
+//        }
+//    }
+//
+////    public class MainButton extends JPanel {
+////
+////        String nimi;
+////
+////        public MainButton(String nimi) {
+////            setSize(50, 50);
+////            this.nimi = nimi;
+////        }
+////        public void paintComponent(Graphics g) {
+////            g. setColor(new Color(150, 150, 150));
+////            super.paintComponent(g);
+////            g. setColor(new Color(0, 0, 0));
+////            g.drawString(nimi, 10, 80);
+////        }
+//
+//
+//}
