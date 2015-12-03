57fb59baea5d2810c8f41936ccee43a6d48150b7
Erik S <Erik S>
2015-12-03 15:46:34 +0200

    Ulesanete graafiline pool

--- src/harjutused/Harjutus.java
+++ src/harjutused/Harjutus.java
@@ -9,19 +9,33 @@ import java.io.IOException;
 public abstract class Harjutus extends JPanel{
 
     public boolean kasAvatud;
+    public JLabel ulesandeNimi = new JLabel();
+    public JLabel kusimus = new JLabel();
+    public JLabel eelvastus = new JLabel();
+    public String kasutajaVastus;
+    public String oigeVastus;
+
+
+
     main.Nupp tagasi = new main.Nupp("Tagasi");
 
     public Harjutus() {
         tagasi.setSize(100, 50);
         tagasi.setLocation(0, 0);
+        ulesandeNimi.setFont(new Font("Serif", Font.BOLD, 28));
+        ulesandeNimi.setSize(176, 28);
+        ulesandeNimi.setForeground(Color.BLACK);
+        ulesandeNimi.setLocation(145, 184);
+
 
         this.setLayout(null);
         this.add(tagasi);
+        this.add(ulesandeNimi);
     }
 
     public void paintComponent(Graphics g) {
         try {
-            Image pilt = (Image) ImageIO.read(getClass().getResourceAsStream("TuhiPilt.jpg"));
+            Image pilt = (Image) ImageIO.read(getClass().getResourceAsStream("TuhiPilt1.jpg"));
             g.drawImage(pilt, 0, 0, this);
         } catch (IOException e) {}
     }
--- src/harjutused/teemad/Arvutusulesanded.java
+++ src/harjutused/teemad/Arvutusulesanded.java
@@ -7,12 +7,14 @@ public class Arvutusulesanded {
         public Ulesanne1() {
             super();
             kasAvatud = true;
+            ulesandeNimi.setText("Ülesanne 1");
         }
     }
     public static class Ulesanne2 extends harjutused.Harjutus {
 
         public Ulesanne2() {
             super();
+            ulesandeNimi.setText("Ülesanne 2");
         }
 
     }
@@ -20,24 +22,28 @@ public class Arvutusulesanded {
 
         public Ulesanne3() {
             super();
+            ulesandeNimi.setText("Ülesanne 3");
         }
     }
     public static class Ulesanne4 extends harjutused.Harjutus {
 
         public Ulesanne4() {
             super();
+            ulesandeNimi.setText("Ülesanne 4");
         }
     }
     public static class Ulesanne5 extends harjutused.Harjutus {
 
         public Ulesanne5() {
             super();
+            ulesandeNimi.setText("Ülesanne 5");
         }
     }
     public static class Ulesanne6 extends harjutused.Harjutus {
 
         public Ulesanne6() {
             super();
+            ulesandeNimi.setText("Ülesanne 6");
         }
     }
 }
